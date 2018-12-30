164515e5ddf2c131bbe1489e2a29cf064c323894
Kyle Kendall <kkendall33@gmail.com>
2018-12-29 23:37:08 -0700

    Port changes for heroku

--- server.js
+++ server.js
@@ -7,20 +7,18 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bodyParser.json({ type: 'application/json' }))
-app.use(bodyParser.json({ type: 'application/x-www-form-urlencoded'}))
+app.use(bodyParser.json({ type: 'application/x-www-form-urlencoded' }))
 
 app.use(express.static(__dirname + '/www'));
 
-app.listen('3000');
-console.log('working on 3000');
+const port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port);
 
-
-// const auth = require('./src/routes/auth');
-// const user = require('./src/routes/user');
-
-
-// app.use('./src/routes/auth', auth);
-// app.use('./src/routes/user', passport.authenticate('jwt', {session: false}), user);
+app.listen(process.env.PORT);
+console.log('working on ' + process.env.PORT);
 
 const passport = require('./passport')(models);
 
