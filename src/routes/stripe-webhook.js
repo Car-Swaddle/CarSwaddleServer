58dbada89529daa9b61aee159fbc1a75584d570f
Kyle Kendall <kkendall33@gmail.com>
2019-01-08 22:39:33 -0700

    Webhooks

--- src/routes/stripe-webhook.js
+++ src/routes/stripe-webhook.js
@@ -5,21 +5,37 @@ const stripe = require('stripe')(constants.STRIPE_SECRET_KEY);
 const liveEndpointSecret = 'whsec_w70LXPKXB954f8H41fKnc7HBIwrBHyoT';
 const testEndpointSecret = 'whsec_Mihcejqv5prmk29eoHGuytmCFOwfDqzG';
 
+function addRawBody(req, res, next) {
+    req.setEncoding('utf8');
+
+    var data = '';
+
+    req.on('data', function (chunk) {
+        data += chunk;
+    });
+
+    req.on('end', function () {
+        req.rawBody = data;
+
+        next();
+    });
+}
+
 module.exports = function (app, models) {
 
-    app.post('/stripe-webhook', function (req, res) {
+    app.post('/stripe-webhook', addRawBody, function (req, res) {
         let sig = req.headers["stripe-signature"];
 
         var event = null;
         try {
-          event = stripe.webhooks.constructEvent(req.body, sig, testEndpointSecret);
+            event = stripe.webhooks.constructEvent(request.rawBody, sig, testEndpointSecret);
         } catch (err) {
-          return res.send(err);
+            return res.send(err);
         }
 
         console.log(event);
-        
-        return res.json({received: true});
+
+        return res.json({ received: true });
     });
 
 };
