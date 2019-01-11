b37490800e71fda7f0d2670c5fa2c724b5f39877
Kyle Kendall <kkendall33@gmail.com>
2019-01-10 21:40:28 -0700

    Reduce max distance

--- src/routes/api/mechanic.js
+++ src/routes/api/mechanic.js
@@ -49,7 +49,7 @@ module.exports = function (router, models) {
         var longitude = parseFloat(query.longitude);
         var limit = query.limit || 10;
         limit = limit > 25 ? 25 : limit;
-        const maxDistance = parseFloat(query.maxDistance) || 10000;
+        const maxDistance = parseFloat(query.maxDistance) || 100000;
 
         models.sequelize.query('SELECT *, u.id as "userID", m.id as "id", r.id as "regionID", ST_Distance(r.origin, ST_MakePoint(?, ?), false) AS "distance" FROM "user" AS u INNER JOIN mechanic as m ON m."userID" = u.id INNER JOIN region AS r ON m.id = r."mechanicID" AND ST_Distance(r.origin, ST_MakePoint(?, ?), false) < r.radius AND m."isActive" = true AND ST_Distance(r.origin, ST_MakePoint(?, ?), false) <= ? ORDER BY ST_MakePoint(?,?) <-> r.origin FETCH FIRST ? ROWS ONLY', {
             replacements: [longitude, latitude, longitude, latitude, longitude, latitude, maxDistance, longitude, latitude, limit],
