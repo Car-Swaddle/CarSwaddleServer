9d509b0c619d48f160a80f016248f65f40aa7ca1
Kyle Kendall <kkendall33@gmail.com>
2019-01-26 16:14:04 -0700

    Return file

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var models = require('./src/models');
 var bodyParser = require('body-parser');
-const busboy = require('connect-busboy');
-const busboyBodyParser = require('busboy-body-parser');
-const fileUpload = require('express-fileupload');
+const path = require('path');
+
+const associationFilePath = 'src/resources/apple-app-site-association';
 
 bodyParser.limit = '500mb';
 
 var app = express();
 app.use(express.static(__dirname + '/www'));
+app.use('/.wellknown/apple-app-site-association', express.static(path.resolve(associationFilePath)));
 
 var port = process.env.PORT;
 if (port == null || port == "") {
--- src/routes/iOSAppFileAssociation.js
+++ src/routes/iOSAppFileAssociation.js
@@ -1,20 +1,12 @@
 const bodyParser = require('body-parser');
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
 
-const associationFilePath = '../resources/apple-app-site-association';
+const associationFilePath = 'src/resources/apple-app-site-association';
 
 module.exports = function (app, models, passport) {
 
-    app.get('/apple-app-site-association', bodyParser.json(), function (req, res, next) {
-        fs.readFile(__dirname + '/' + associationFilePath, 'utf8', function (err, data) {
-            if (data == null) {
-                console.log(err);
-                res.status(404).send();
-                return;
-            }
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.write(data, 'binary');
-            res.end(null, 'binary');
-        });
+    app.get('/someTest/apple-app-site-association', bodyParser.json(), function (req, res, next) {
+        return res.sendfile(path.resolve(associationFilePath));
     });
 };
