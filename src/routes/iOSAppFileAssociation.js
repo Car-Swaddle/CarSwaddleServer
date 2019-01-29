d2818ee03003164db3e145f362205c8b5deae758
Kyle Kendall <kkendall33@gmail.com>
2019-01-28 21:28:14 -0700

    Add actual file

--- src/routes/iOSAppFileAssociation.js
+++ src/routes/iOSAppFileAssociation.js
@@ -6,12 +6,12 @@ const associationFilePath = 'src/resources/apple-app-site-association';
 
 module.exports = function (app, models, passport) {
 
-    app.get('/.well-known/apple-app-site-association', bodyParser.json(), function (req, res, next) {
+    app.get('/notanything/.well-known/apple-app-site-association', bodyParser.json(), function (req, res, next) {
         res.setHeader("Content-Type", "application/json");
         return res.download(path.resolve(associationFilePath));
     });
 
-    app.get('/apple-app-site-association', bodyParser.json(), function (req, res, next) {
+    app.get('/notanything/apple-app-site-association', bodyParser.json(), function (req, res, next) {
         res.setHeader("Content-Type", "application/json");
         return res.download(path.resolve(associationFilePath));
         // return res.sendFile(path.resolve(associationFilePath));
