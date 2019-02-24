387eb78c7e073819172fe84c43e05a3b3f5fad78
Kyle Kendall <kkendall33@gmail.com>
2019-02-23 23:44:44 -0700

    fix mechanic

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var models = require('./src/models');
 var bodyParser = require('body-parser');
-const path = require('path');
-
-const associationFilePath = 'src/resources/apple-app-site-association';
 
 bodyParser.limit = '500mb';
 
@@ -19,11 +16,11 @@ app.use(express.static(__dirname + '/www'));
 
 var port = process.env.PORT;
 if (port == null || port == "") {
-    port = 3000;
+    port = 5432;
 }
 app.listen(port);
 
-console.log('working on ' + process.env.PORT);
+console.log('working on ' + port);
 
 const passport = require('./passport')(models);
 
--- src/routes/api/mechanic.js
+++ src/routes/api/mechanic.js
@@ -52,7 +52,7 @@ module.exports = function (router, models) {
         limit = limit > 25 ? 25 : limit;
         const maxDistance = parseFloat(query.maxDistance) || 100000;
 
-        models.sequelize.query('SELECT *, u.id as "userID", m.id as "id", r.id as "regionID", ST_Distance(r.origin, ST_MakePoint(?, ?), false) AS "distance" FROM "user" AS u INNER JOIN mechanic as m ON m."userID" = u.id INNER JOIN region AS r ON m.id = r."mechanicID" AND ST_Distance(r.origin, ST_MakePoint(?, ?), false) < r.radius AND m."isActive" = true AND m."isAllowed" = true AND ST_Distance(r.origin, ST_MakePoint(?, ?), false) <= ? ORDER BY ST_MakePoint(?,?) <-> r.origin FETCH FIRST ? ROWS ONLY', {
+        models.sequelize.query('SELECT *, u.id as "userID", m.id as "id", r.id as "regionID", ST_Distance(r.origin, ST_MakePoint(?, ?), false) AS "distance" FROM "user" AS u INNER JOIN mechanic as m ON m."userID" = u.id INNER JOIN region AS r ON m.id = r."mechanicID" AND ST_Distance(r.origin, ST_MakePoint(?, ?), false) < r.radius AND m."isActive" = true AND ST_Distance(r.origin, ST_MakePoint(?, ?), false) <= ? ORDER BY ST_MakePoint(?,?) <-> r.origin FETCH FIRST ? ROWS ONLY', {
             replacements: [longitude, latitude, longitude, latitude, longitude, latitude, maxDistance, longitude, latitude, limit],
             type: models.sequelize.QueryTypes.SELECT,
             model: models.User
