0044719dff66a925e952c09675ac05ed7a5b4a94
Kyle Kendall <kkendall33@gmail.com>
2019-04-20 03:02:48 -0600

    Remove Reset of DB

--- src/models/index.js
+++ src/models/index.js
@@ -51,7 +51,7 @@ Object.keys(models).forEach(key => {
 });
 
 // {force: true}
-sequelize.sync({force: true}).then( function() {
+sequelize.sync().then( function() {
   console.log("synced")
   const reminder = new Reminder(models);
   reminder.rescheduleRemindersForAllAutoServices();
