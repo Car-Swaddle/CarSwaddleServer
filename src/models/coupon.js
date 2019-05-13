5ffe4e5f60704be6bfcfa1bbad8f2a29e95f34b3
Kyle Kendall <kkendall33@gmail.com>
2019-05-12 23:09:21 -0600

    Add admin attribute migration

--- src/models/coupon.js
+++ src/models/coupon.js
@@ -1,38 +1,38 @@
-const coupon = function (sequelize, DataTypes) {
-    const Coupon = sequelize.define('coupon', {
-        id: {
-            type: DataTypes.STRING,
-            primaryKey: true,
-            unique: true
-        },
-        code: {
-            type: DataTypes.STRING
-        },
-        type: {
-            type: DataTypes.STRING
-        },
-        value: {
-            type: DataTypes.FLOAT
-        },
-        expireDate: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-    }, {
-        freezeTableName: true,
-    });
+// const coupon = function (sequelize, DataTypes) {
+//     const Coupon = sequelize.define('coupon', {
+//         id: {
+//             type: DataTypes.STRING,
+//             primaryKey: true,
+//             unique: true
+//         },
+//         code: {
+//             type: DataTypes.STRING
+//         },
+//         type: {
+//             type: DataTypes.STRING
+//         },
+//         value: {
+//             type: DataTypes.FLOAT
+//         },
+//         expireDate: {
+//             type: DataTypes.DATE,
+//             allowNull: true
+//         },
+//     }, {
+//         freezeTableName: true,
+//     });
 
-    Coupon.associate = models => {
-        Coupon.belongsTo(models.Price, { foreignKey: 'priceID' });
-        Coupon.belongsTo(models.Mechanic, { foreignKey: 'mechanicID' });
-    };
+//     Coupon.associate = models => {
+//         Coupon.belongsTo(models.Price, { foreignKey: 'priceID' });
+//         Coupon.belongsTo(models.Mechanic, { foreignKey: 'mechanicID' });
+//     };
 
-    Coupon.COUPONT_TYPE = {
-        user: 'user',
-        admin: 'admin',
-    };
+//     Coupon.COUPONT_TYPE = {
+//         user: 'user',
+//         admin: 'admin',
+//     };
 
-    return Coupon;
-};
+//     return Coupon;
+// };
 
-module.exports = coupon;
+// module.exports = coupon;
