d2060818b3e173fff9cd0b01cd2e66e222554885
Kyle Kendall <kkendall33@gmail.com>
2019-05-28 00:04:22 -0600

    Force reset db

--- src/models/index.js
+++ src/models/index.js
@@ -54,7 +54,7 @@ Object.keys(models).forEach(key => {
 });
 
 // {force: true}
-sequelize.sync().then( function() {
+sequelize.sync({force: true}).then( function() {
   console.log("synced")
   const reminder = new Reminder(models);
   reminder.rescheduleRemindersForAllAutoServices();
