764f9599c0837a252eeeb488351f65af315a1154
Brad Richardson <bradrichardsonmail@gmail.com>
2021-03-01 23:17:37 -0700

    Add files

---
+++ src/routes/api/referrer.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const constants = require('../../controllers/constants');
+const bodyParser = require('body-parser');
+
+module.exports = (router, models) => {
+    
+    router.get('/referrers', bodyParser.json(), async function (req, res) {
+        return res.json([]);
+    });
+
+    router.get('/referrers/:referrerID', bodyParser.json(), async function (req, res) {
+        // req.params.referrerID
+        return res.json([]);
+    });
+
+    router.post('/referrers', bodyParser.json(), async function (req, res) {
+        return res.json({});
+    });
+
+    router.put('/referrers/:referrerID', bodyParser.json(), async function (req, res) {
+        return res.json({});
+    });
+
+    router.delete('/referrers/:referrerID', bodyParser.json(), async function (req, res) {
+        return res.json({});
+    });
+
+    return router;
+};
+
---
+++ src/util/pino-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    customLogLevel: function (res, err) {
+        if (res.statusCode >= 400 && res.statusCode < 500) {
+            return 'warn'
+        } else if (res.statusCode >= 500 || err) {
+            return 'error'
+        }
+        return 'info'
+    },
+    serializers: {
+        req: function customReqSerializer (req) {
+            delete req.headers;
+            return req;
+        },
+        res: function customResSerializer (res) {
+            delete res.headers;
+            if (res && res.raw && res.raw.req && res.raw.req.user) {
+                res.userId = res.raw.req.user.id;
+            }
+            return res;
+        }
+    }
+};
