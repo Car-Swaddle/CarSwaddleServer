89ad4b35cb41e89071fed89f05ff1c22ce1ec282
Brad Richardson <bradrichardsonmail@gmail.com>
2021-03-30 22:47:31 -0600

    Add various services, routes, required deps

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { authenticationService } from './_services/authenticationService';
+import ProtectedRoute from './_components/ProtectedRoute';
+import { LoginPage } from './LoginPage/LoginPage';
+
+class App extends React.Component<{}, {currentToken: string | null}> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            currentToken: null,
+        };
+    }
+
+    componentDidMount() {
+      authenticationService.currentToken.subscribe(x => this.setState({ currentToken: x }));
+    }
+
+    logout() {
+        authenticationService.logout();
+        // history.push('/login');
+    }
+
+    render() {
+        const { currentToken } = this.state;
+        return (
+            <BrowserRouter>
+                <div>
+                    {currentToken &&
+                        <nav className="navbar navbar-expand navbar-dark bg-dark">
+                            <div className="navbar-nav">
+                                <Link to="/" className="nav-item nav-link">Home</Link>
+                                <a onClick={this.logout} className="nav-item nav-link">Logout</a>
+                            </div>
+                        </nav>
+                    }
+                    <div className="jumbotron">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-6 offset-md-3">
+                                    <ProtectedRoute isAuthenticated={(currentToken && currentToken.length>0)===true} authenticationPath="/" />
+                                    <Route path="/login" component={LoginPage} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
-export default App;
+export { App }; 
---
+++ web/src/_helpers/handleResponse.ts
@@ -0,0 +1,17 @@
+import { authenticationService } from '../_services/authenticationService';
+
+export function handleResponse(response: any) {
+    return response.json().then((data: any) => {
+        if (!response.ok) {
+            if (401 === response.status) {
+                // auto logout if 401 Unauthorized response returned from api
+                authenticationService.logout();
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
+}
