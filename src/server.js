e6a2c3994b11020c044620b09214b12800a09291
Brad Richardson <bradrichardsonmail@gmail.com>
2021-04-19 20:43:57 -0600

    Cookie parser

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const models = require('./models');
 const constants = require('./controllers/constants');
 const stripe = require("stripe")(constants.STRIPE_SECRET_KEY);
 const pino = require('pino-http')(require('./util/pino-config'));
+const cookieParser = require('cookie-parser');
+const { reservationsUrl } = require('twilio/lib/jwt/taskrouter/util');
 
 stripe.setAppInfo({
     name: "Car Swaddle Server Stripe Library",
@@ -17,6 +19,7 @@ console.log(__dirname)
 
 const app = express();
 app.use(pino);
+app.use(cookieParser())
 
 express.static.mime.define({'application/pkcs7-mime': ['apple-app-site-association']});
 express.static.mime.define({'application/pkcs7-mime': ['.well-known/apple-app-site-association']});
