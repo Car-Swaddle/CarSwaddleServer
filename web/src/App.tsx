0dd9882e87649b090ffaa233ef219ca3f4254a38
Brad Richardson <brad-richardson@users.noreply.github.com>
2021-04-28 23:34:25 -0600

    Working login redirect (#28)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,55 +1,33 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom';
 
-import { authenticationService } from './_services/authenticationService';
-import ProtectedRoute from './_components/ProtectedRoute';
-import LoginPage from './LoginPage/LoginPage';
+import { authenticationService } from './services/authenticationService';
+import LoginPage from './components/LoginPage';
+import AffiliateDashboard from './components/AffiliateDashboard';
 
-class App extends React.Component<{}, {currentToken: string | null}> {
-    constructor(props: any) {
-        super(props);
+export default function App() {
+    const [authenticated, setAuthenticated] = React.useState(authenticationService.isAuthenticated());
 
-        this.state = {
-            currentToken: null,
-        };
+    function finishedAuth() {
+        setAuthenticated(true);
     }
 
-    componentDidMount() {
-        
-    }
-
-    logout() {
-        authenticationService.logout();
-        // history.push('/login');
-    }
-
-    render() {
-        const { currentToken } = this.state;
-        return (
-            <BrowserRouter>
-                <div>
-                    {currentToken &&
-                        <nav className="navbar navbar-expand navbar-dark bg-dark">
-                            <div className="navbar-nav">
-                                <Link to="/" className="nav-item nav-link">Home</Link>
-                                <a onClick={this.logout} className="nav-item nav-link">Logout</a>
-                            </div>
-                        </nav>
-                    }
-                    <div className="jumbotron">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-6 offset-md-3">
-                                    <ProtectedRoute isAuthenticated={(currentToken && currentToken.length>0)===true} authenticationPath="/" />
-                                    <Route path="/login" component={LoginPage} />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </BrowserRouter>
-        );
-    }
+    return (
+        <BrowserRouter>
+            {!authenticated
+                ? <>
+                <Route path="/login">
+                    <LoginPage finishedAuth={finishedAuth}/>
+                </Route>
+                <Redirect to="/login" />
+                </>
+                : <>
+                <Route path="/affiliate">
+                    <AffiliateDashboard />
+                </Route>
+                <Redirect from="*" to="/affiliate" />
+                </>
+            }
+        </BrowserRouter>
+    );
 }
-
-export { App }; 
---
+++ web/src/components/LoginPage.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import { authenticationService } from '../services/authenticationService';
+import { Container, Row, Col, Form, Button, Jumbotron } from 'react-bootstrap';
+
+export type LoginPageProps = {
+    finishedAuth: () => void,
+}
+
+export default function LoginPage({finishedAuth}: LoginPageProps) {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event: any) {
+        event.preventDefault();
+        authenticationService.login(email, password)
+        .then(token => {
+            if (token && token.length) {
+                finishedAuth()
+            } else {
+                // TODO - error handling
+            }
+        })
+    }
+
+    return (
+        <Container>
+            <br/>
+            <Row>
+                <Col sm={{span: 6, offset: 3}} lg={{span: 4, offset: 4}}>
+                    <h3 className="text-center">Car Swaddle Affiliate</h3>
+                </Col>
+            </Row>
+            <br/>
+            <Row>
+                <Col sm={{span: 6, offset: 3}} lg={{span: 4, offset: 4}}>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Control autoFocus type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" disabled={!validateForm()} block>
+                        Login
+                    </Button>
+                </Form>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
