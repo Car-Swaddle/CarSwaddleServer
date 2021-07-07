50cd9bb22bd3ec0c26d7d35ed17b9ff5909425aa
Brad Richardson <bradrichardsonmail@gmail.com>
2021-07-07 00:55:36 -0600

    Cleanup, re-add heroku handlers

--- src/start.ts
+++ src/start.ts
@@ -7,3 +7,23 @@ if (port == null || port == "") {
 app.listen(port);
 
 console.log('working on ' + port);
+
+// Gracefully handle heroku reboots
+process.on('SIGTERM', signalListener()).on('SIGINT', signalListener());
+
+function signalListener(): (signal: NodeJS.Signals) => void {
+    return (signal) => {
+        console.warn(`Received ${ signal }...`);
+        setTimeout(() => {
+            console.log('...waited 10s, exiting.');
+            process.exit(0);
+        }, 10_000).unref();
+    };
+}
+
+process.on('uncaughtException', (err: Error) => {
+    console.error(err?.stack ?? err);
+    process.exit(err ? 1 : 0);
+});
+
+process.on('warning', (warning: Error) => {console.warn(warning.message); console.warn(warning.stack)});
